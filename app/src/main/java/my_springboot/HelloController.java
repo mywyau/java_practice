7b50c73016ddc9b0fc0900c37a2e3d64f051a421
mywyau <mywyaudev@gmail.com>
2025-03-05 10:17:21 +0800

    add index page

--- app/src/main/java/my_springboot/HelloController.java
+++ app/src/main/java/my_springboot/HelloController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "Hello this is the index page";
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Spring Boot!";
     }
+
 }
