e8abd969fe52bee3802906867887577722170b49
mywyau <mywyaudev@gmail.com>
2025-03-05 17:54:30 +0800

    split run script and test apps

---
+++ run _exercises.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop the script if any command fails
+set -e
+
+# Build the application
+echo "Building the application..."
+./gradlew build
+
+# Run the application
+echo "Starting the application..."
+./gradlew bootRun
