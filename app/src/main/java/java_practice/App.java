89a539c0b43bec220fed73f60ec2f5a177d0c551
mywyau <mywyaudev@gmail.com>
2025-03-05 18:20:10 +0800

    add java basics

--- app/src/main/java/java_practice/App.java
+++ app/src/main/java/java_practice/App.java
@@ -3,13 +3,23 @@ package java_practice;
 
 public class App {
 
-
-    
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+
+        ControlFlow controlFlow = new ControlFlow();
+        ForLoops forLoops = new ForLoops();        
+        Dog myDog = new Dog("Buddy", 3);
+        SwitchStatements switchStatements = new SwitchStatements();
+
         System.out.println(new App().getGreeting());
+
+        controlFlow.ifElseMethod();
+        myDog.bark();
+        forLoops.printNumbers();
+        
+        System.out.println(switchStatements.dayName);
     }
 }
---
+++ app/src/main/java/java_practice/SwitchStatements.java
@@ -0,0 +1,13 @@
+package java_practice;
+
+public class SwitchStatements {
+
+    int day = 3;
+
+    String dayName = switch (day) {
+        case 1 -> "Monday";
+        case 2 -> "Tuesday";
+        case 3 -> "Wednesday";
+        default -> "Unknown";
+    };
+}
