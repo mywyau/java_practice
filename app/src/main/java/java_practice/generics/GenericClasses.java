70b0d29a3958924c7b2bdff89be6f860ee9fcd7e
mywyau <mywyaudev@gmail.com>
2025-03-07 11:26:04 +0800

    add generic classes code

--- app/src/main/java/java_practice/generics/GenericClasses.java
+++ app/src/main/java/java_practice/generics/GenericClasses.java
@@ -5,7 +5,7 @@ public class GenericClasses {
 
     // We will define a generic class called Box
 
-    public class Box<T> {
+    public static class Box<T> {
 
         private T item;
 
@@ -16,6 +16,14 @@ public class GenericClasses {
         public T getItem(){
             return this.item; 
         }
+    }
+
+
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>();
+        stringBox.setItem("Sword");
+        String value = stringBox.getItem();
+        System.out.println("Inside my box there is a: " + value);
     }
 
 }
