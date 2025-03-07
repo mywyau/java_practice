61cd5a6f23816c48758b6de9b5cacaa325ce6136
mywyau <mywyaudev@gmail.com>
2025-03-07 12:00:22 +0800

    wip

--- app/src/main/java/java_practice/error_handling/ErrorHandling.java
+++ app/src/main/java/java_practice/error_handling/ErrorHandling.java
@@ -8,16 +8,29 @@ public class ErrorHandling {
 
         try {
             // This line may throw ArrayIndexOutOfBoundsException
-            System.out.println(numbers[5]);
+            System.out.println(numbers[5]);    // we do not hit this due to Array Out of Bounds Exception
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught an ArrayIndexOutOfBoundsException!");
+            System.out.println("\nCaught an ArrayIndexOutOfBoundsException!");
             System.out.println("Error message: " + e.getMessage());
         }
 
         System.out.println("Program continues after catching the exception.");
     }
 
+    static void usingFinally() {
+
+        try {
+            int result = 10 / 0;
+            System.out.println("Result: " + result);  // does not execute since we hit an Arithmetic Exception due to divide by 0.
+        } catch (ArithmeticException e) {
+            System.out.println("\nCaught Arithmetic Exception: " + e.getMessage());
+        } finally {
+            System.out.println("This is the finally block. It always executes.");
+        }
+    }
+
     public static void main(String[] args) {
         basicTryCatchExample();
+        usingFinally();
     }
 }
