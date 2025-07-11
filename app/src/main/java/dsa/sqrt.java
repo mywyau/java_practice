388b9e0cebea7c3c88f8d0eba6496c5a948df475
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-07-11 22:09:02 +0100

    wip

--- app/src/main/java/dsa/sqrt.java
+++ app/src/main/java/dsa/sqrt.java
@@ -13,19 +13,8 @@ public class Sqrt {
     }
 
     public static void main(String[] args) {
-
-        ControlFlow controlFlow = new ControlFlow();
-        Dog myDog = new Dog("Buddy", 3);
-        SwitchStatements switchStatements = new SwitchStatements();
-
-        While whileLoop = new While();
-
-        System.out.println(new App().getGreeting());
-
-        controlFlow.ifElseMethod();
-        myDog.bark();
-        whileLoop.whileLoop();
         
+
         System.out.println(switchStatements.dayName);
     }
 }
